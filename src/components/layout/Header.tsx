b7b7ad5c504509e298be06e6b61d13a2f0fcf9cd
CapelleGab <gabin.capelle.pro@gmail.com>
2025-03-14 19:05:58 +0100

    📦 UPDATE : Link hub

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,16 @@
 "use client";
 import { LoginButton, LogoutButton } from "@/components/auth/AuthButton";
 import { Session } from "next-auth";
+import Link from "next/link";
 import { ModeToggle } from "../theme/theme-mode-toggle";
 
 export default function Header({ session }: { session: Session | null }) {
   const user = session?.user;
   return (
     <header className="flex items-center justify-between px-4 py-2 border-b border-accent">
-      <p>CollabHub</p>
+      <Link href="/" className="text-primary">
+        CollabHub
+      </Link>
 
       <div className="flex items-center gap-2">
         {!user ? <LoginButton /> : <LogoutButton username={user?.name ?? ""} />}
