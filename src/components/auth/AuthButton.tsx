729700522cc4b80cfeeee808e90e772039c3cfa9
CapelleGab <gabin.capelle.pro@gmail.com>
2025-03-14 17:54:07 +0100

    📦 Update : Add Dropdown menu t

--- src/components/auth/AuthButton.tsx
+++ src/components/auth/AuthButton.tsx
@@ -1,6 +1,15 @@
 "use client";
+import { LogOut, User } from "lucide-react";
 import { signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
 export const LoginButton = () => {
   const handleCLick = async () => {
@@ -11,12 +20,32 @@ export const LoginButton = () => {
 };
 
 export const LogoutButton = ({ username }: { username: string }) => {
-  const handleCLick = async () => {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
     await signOut();
   };
+
+  const handleProfileClick = () => {
+    router.push(`/profile`);
+  };
+
   return (
-    <Button variant="destructive" onClick={handleCLick}>
-      {username}
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">{username}</Button>
+      </DropdownMenuTrigger>  
+      <DropdownMenuContent>
+        <DropdownMenuItem onClick={handleProfileClick}>
+          <User className="mr-2 h-4 w-4" />
+          Profile
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem variant="destructive" onClick={handleSignOut}>
+          <LogOut className="mr-2 h-4 w-4" />
+          Sign out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
